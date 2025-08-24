4e4edc5e68a61c3a4a9020a9119058717a4c0654
Shahar Belkind <shaharbelkind@Shahars-MacBook-Air.local>
2025-08-24 19:00:54 +0300

    Fix providers.tsx to remove Apollo client dependency

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React from 'react';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './apollo-client';
-export default function Providers({children}:{children:React.ReactNode}){ return <ApolloProvider client={client}>{children}</ApolloProvider>; }
+
+export default function Providers({children}:{children:React.ReactNode}){ 
+  return <>{children}</>; 
+}
